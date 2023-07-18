73f2118a587bf65c4f7a007fa77784af95bea24b
JustSemen <semyn03@mail.ru>
2023-07-18 21:28:03 +0300

    init

---
+++ src/index.ts
@@ -0,0 +1,35 @@
+import express from 'express'
+const fetch = (...args) => import('node-fetch').then(({ default: fetch }) => fetch(...args));
+
+const app = express();
+const port = 3000;
+
+
+
+app.get('/', async (request, response) => {
+
+    const wbResponse = await fetch('https://card.wb.ru/cards/detail?appType=1&curr=rub&nm=138593051;94340317;94340606;138590435;138607462;94339119;94339244;&dest=-1681991')
+        .then(res => res.json())
+
+    // Сложность алгоритма - O(n)^2, иначе по-моему никак
+    const mappedProducts = wbResponse.data.products.map((product) => {
+        const mappedSizes = []
+        product.sizes.forEach(size => {
+            if (size.stocks.length > 0) {
+                mappedSizes.push(size.name)
+            }
+        })
+
+        return {
+            Art: product.id,
+            sizes: mappedSizes
+        }
+    })
+
+    response.send(mappedProducts)
+    // Сомневаюсь только что это товары именно из Казани, но за склад точно отвечает параметр dest
+    // получил значение -1681991 опытным путём - тыкал на товары, где написано, что доставка со склада Казань WB и этот параметр всегда совпадал
+    // в api селлера есть эндпоинт чтобы получить все склады wb, но там нужен токен селлера, а как селлер я зарегаться не смог.
+});
+
+app.listen(port, () => console.log(`Running on port ${port}`));
